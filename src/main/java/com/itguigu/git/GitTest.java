10f18e94cc778b86c7d6f529eb959d3150ac20f9
Sam <1024637587@qq.com>
2022-04-04 09:29:33 +0800

    hot-fix commit2

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git11");
         System.out.println("hello git12");
         System.out.println("hello git14");
+        System.out.println("hot-fix test");
     }
 }
